ef38110cd9cadfffefc22450f6583da68bf06b68
Thomas Hodges <thodges@javascriptonline.org>
2017-01-20 18:13:38 -0800

    squares

---
+++ squares.py
@@ -0,0 +1,18 @@
+# squares = []
+# for value in range(1,11):
+# 	square = value**2
+# 	squares.append(square)
+
+# print(squares)
+
+squares = []
+for value in range(1,11):
+	squares.append(value**2)
+
+print(squares)
+
+digits = list(range(0,10))
+print(digits)
+print("min: " + str(min(digits)))
+print("max: " + str(max(digits)))
+print("sum: " + str(sum(digits)))
