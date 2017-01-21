8f54b75f1ca6a01ff36a0d34862876974661ccae
Thomas Hodges <thodges@javascriptonline.org>
2017-01-20 18:44:35 -0800

    foods

---
+++ foods.py
@@ -0,0 +1,12 @@
+my_foods = ['pizza', 'falafel', 'carrot cake']
+friend_foods = my_foods[:]
+
+my_foods.append("cannoli")
+
+friend_foods.append("iced cream")
+
+print("My favourite foods are: ")
+print(my_foods)
+
+print("\nMy friend's favourite foods are: ")
+print(friend_foods)
