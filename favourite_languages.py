af732062db73698b8771aabcd52d8a597bb8b25b
Thomas Hodges <thodges@javascriptonline.org>
2017-01-21 17:33:46 -0800

    favourite_languages

---
+++ favourite_languages.py
@@ -0,0 +1,8 @@
+favourite_languages = {
+	'jen':'python',
+	'sarah':'c',
+	'edward':'ruby',
+	'phil':'python'
+}
+
+print("Sarah's favourite language is " + favourite_languages['sarah'].title() + '.')
