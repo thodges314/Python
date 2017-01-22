55ee8427eeeac10b88eda448df58159c63f5a029
Thomas Hodges <thodges@javascriptonline.org>
2017-01-21 19:25:21 -0800

    even_or_odd

---
+++ even_or_odd.py
@@ -0,0 +1,6 @@
+number = int(input("Enter a number and I'll tell you if it is even or odd: "))
+
+if number % 2:
+	print("The number " + str(number) + " is odd.")
+else: 
+	print("The number " + str(number) + " is even.")
--- rollercoaster.py
+++ rollercoaster.py
@@ -1,6 +1,6 @@
 height = int(input("How tall are you, in inches? "))
 
 if height >= 36:
-	print("\nYou are tall enough to ride!")
+	print("\nYou are tall enough to ride!")fg
 else:
 	print("\nGet the hell out of here, little brat!")
